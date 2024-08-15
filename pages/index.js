78d57bcd221970acda6a7b1a24bced97387da313
desamtralized <sambarbosaa@gmail.com>
2024-08-15 14:48:06 -0500

    remove unused components

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 import Head from "next/head";
 import GradientWrapper from "../components/GradientWrapper";
-import CTA from "../components/ui/CTA";
-import Features from "../components/ui/Features";
 import FooterCTA from "../components/ui/FooterCTA";
 import Hero from "../components/ui/Hero";
 import Testimonials from "../components/ui/Testimonials";
